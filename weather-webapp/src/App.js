109f7c72016341885ce577a7c739229ae4ceee8b
Saad <33384287+iamsaadb@users.noreply.github.com>
2019-06-03 02:04:51 -0700

    props and states set && API result fully functional (city, country,temp...)

--- weather-webapp/src/App.js
+++ weather-webapp/src/App.js
@@ -6,19 +6,46 @@ import Weather from "./components/Weather";
 const API_KEY = "166500be60a2e38e450a103d5d20b7ef";
 
 class App extends React.Component {
-  getWeather = async () => {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=oakland,ca&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`
     );
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   };
   render() {
     return (
       <div>
         <Title />
-        <Form />
-        <Weather />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
